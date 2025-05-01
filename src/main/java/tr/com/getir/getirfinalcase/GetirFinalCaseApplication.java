2e85ca23cdf6b4d6bb82059a1ba4ea334efa7516
İlknur Doğan <ilknurdogan254@gmail.com>
2025-05-01 16:13:30 +0300

    Initial commit

---
+++ src/main/java/tr/com/getir/getirfinalcase/GetirFinalCaseApplication.java
@@ -0,0 +1,13 @@
+package tr.com.getir.getirfinalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirFinalCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetirFinalCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/getir/getirfinalcase/GetirFinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.getir.getirfinalcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetirFinalCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
