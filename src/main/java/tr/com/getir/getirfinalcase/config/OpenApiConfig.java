6647e3bb9151ceeb2e0ed21c666d73526f0583ac
İlknur Doğan <ilknurdogan254@gmail.com>
2025-05-05 18:14:38 +0300

    feat: add OpenAPI configuration

---
+++ src/main/java/tr/com/getir/getirfinalcase/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package tr.com.getir.getirfinalcase.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Management System API",
+                description = "OpenAPI documentation for the Getir Java Spring Bootcamp Final Case Project",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Durdu İlknur Doğan",
+                        email = "ilknurdogan254@gmail.com"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(description = "Local Environment", url = "http://localhost:8080")
+        },
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        description = "Provide the JWT token. Format: Bearer <token>"
+)
+public class OpenApiConfig {
+}
