3cf82f8265e4d91e104d08497c6933c759872368
İlknur Doğan <ilknurdogan254@gmail.com>
2025-05-10 16:17:42 +0300

    feat: create BorrowRecordRepository with overdue check method

---
+++ src/main/java/tr/com/getir/getirfinalcase/repository/BorrowRecordRepository.java
@@ -0,0 +1,14 @@
+package tr.com.getir.getirfinalcase.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tr.com.getir.getirfinalcase.model.entity.BorrowRecord;
+import tr.com.getir.getirfinalcase.model.entity.User;
+
+import java.time.LocalDate;
+
+@Repository
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+
+    boolean existsByUserAndDueDateBeforeAndReturnDateIsNull(User user, LocalDate now);
+}
