649b4d42c843731197f677c03d37b2be732f290c
İlknur Doğan <ilknurdogan254@gmail.com>
2025-05-11 23:13:25 +0300

    feat: add unit tests for login method in AuthenticationServiceImpl

--- src/test/java/tr/com/getir/getirfinalcase/service/impl/AuthenticationServiceImplTest.java
+++ src/test/java/tr/com/getir/getirfinalcase/service/impl/AuthenticationServiceImplTest.java
@@ -6,22 +6,25 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import tr.com.getir.getirfinalcase.exception.EmailAlreadyExistException;
+import tr.com.getir.getirfinalcase.exception.InvalidCredentialsException;
 import tr.com.getir.getirfinalcase.mapper.UserMapper;
 import tr.com.getir.getirfinalcase.model.dto.request.UserCreateRequest;
+import tr.com.getir.getirfinalcase.model.dto.request.UserLoginRequest;
 import tr.com.getir.getirfinalcase.model.dto.response.AuthenticationResponse;
 import tr.com.getir.getirfinalcase.model.entity.User;
 import tr.com.getir.getirfinalcase.model.enums.UserRole;
 import tr.com.getir.getirfinalcase.repository.UserRepository;
+import tr.com.getir.getirfinalcase.security.CustomUserDetails;
 import tr.com.getir.getirfinalcase.security.CustomUserDetailsService;
 import tr.com.getir.getirfinalcase.security.JwtUtil;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationServiceImplTest {
@@ -46,15 +49,19 @@ class AuthenticationServiceImplTest {
     // REGISTER - Success
     @Test
     void shouldRegisterUserAndReturnToken_whenEmailIsUnique() {
+        // GIVEN
         UserCreateRequest request = createMockUserRequest();
         User user = createMockUser();
 
+        // Mock the calls
         when(userRepository.findByEmail(request.email())).thenReturn(Optional.empty());
         when(userMapper.mapUserCreateRequestToUser(request)).thenReturn(user);
         when(jwtUtil.generateToken(any())).thenReturn("mocked-token");
 
+        // WHEN
         AuthenticationResponse response = authenticationService.register(request);
 
+        // THEN
         assertNotNull(response);
         assertEquals("mocked-token", response.token());
         verify(userRepository).save(user);
@@ -64,10 +71,47 @@ class AuthenticationServiceImplTest {
     // REGISTER - Failure
     @Test
     void shouldThrowEmailAlreadyExistException_whenEmailAlreadyExists() {
+        // GIVEN
         UserCreateRequest request = createMockUserRequest();
+
+        // Mock the calls
         when(userRepository.findByEmail(request.email())).thenReturn(Optional.of(new User()));
 
+        // WHEN & THEN
         assertThrows(EmailAlreadyExistException.class, () -> authenticationService.register(request));
+    }
+
+    // LOGIN - Success
+    @Test
+    void shouldReturnToken_whenLoginIsSuccessful() {
+        // WHEN & THEN
+        UserLoginRequest request = createMockLoginRequest();
+        User user = createMockUser();
+
+        // Mock the calls
+        when(customUserDetailsService.loadUserByUsername(request.email())).thenReturn(new CustomUserDetails(user));
+        when(jwtUtil.generateToken(any())).thenReturn("login-token");
+
+        // WHEN
+        AuthenticationResponse response = authenticationService.login(request);
+
+        // THEN
+        assertNotNull(response);
+        assertEquals("login-token", response.token());
+    }
+
+
+    // Login - Failure
+    @Test
+    void shouldThrowInvalidCredentialsException_whenCredentialsAreWrong() {
+        // GIVEN
+        UserLoginRequest request = createMockLoginRequest();
+
+        doThrow(new BadCredentialsException("invalid")).when(authenticationManager)
+                .authenticate(any(UsernamePasswordAuthenticationToken.class));
+
+        // WHEN & THEN
+        assertThrows(InvalidCredentialsException.class, () -> authenticationService.login(request));
     }
 
     // MOCK DATA
@@ -94,5 +138,8 @@ class AuthenticationServiceImplTest {
         );
     }
 
+    private UserLoginRequest createMockLoginRequest() {
+        return new UserLoginRequest("ilknur@example.com", "123456789");
+    }
 
 }
